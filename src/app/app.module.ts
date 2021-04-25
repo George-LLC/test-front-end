0a804e02824693cdbbefd49a58e145cb02df972d
Gevorg Melkonyan <gevor12@gmail.com>
2021-04-26 00:45:37 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { CreateNewContentComponent } from './create-new-content/create-new-content.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DropdownModule} from 'primeng/dropdown';
+import {ButtonModule} from 'primeng/button';
+import {RippleModule} from 'primeng/ripple';
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {RadioButtonModule} from 'primeng/radiobutton';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateNewContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    ButtonModule,
+    RippleModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    RadioButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/create-new-content/create-new-content.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {PrimeNGConfig, SelectItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-create-new-content',
+  templateUrl: './create-new-content.component.html',
+  styleUrls: ['./create-new-content.component.css']
+})
+export class CreateNewContentComponent implements OnInit {
+  formCreateNew: FormGroup;
+  items: SelectItem[];
+
+  item: string;
+  value4: number = 50;
+  selectedOptions = [
+    {
+      name: 'Bilingual',
+      text: '(add 20%)',
+      add: 20
+    },
+    {
+      name: 'Proof-reading',
+      text: '(add 10%)',
+      add: 10
+    },
+    {
+      name: 'Copy-editinig',
+      text: '(add $20)',
+      add: 20
+    }
+  ]
+
+
+  constructor(private primengConfig: PrimeNGConfig) {
+
+  }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+
+    this.formCreateNew = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      nmbrWriters: new FormControl(''),
+      selectedOpt: new FormControl(''),
+      value4: new FormControl(''),
+      // LastName: new FormControl("", [Validators.required]),
+      // MiddleName: new FormControl("", [Validators.required]),
+      // CompanyName: new FormControl("", [Validators.required]),
+    });
+
+    this.items = [];
+    for (let i = 1; i <= 15; i++) {
+      this.items.push({label: 'Item ' + i, value: 'Item ' + i});
+    }
+  }
+
+}
